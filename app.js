d9f6245fa571dacba3d06daba60c517c74f5952b
Bahodirjon <Bahodirjon@gmail.com>
2024-12-17 15:15:02 +0200

    4.5-dars vazifam

---
+++ app.js
@@ -0,0 +1,61 @@
+// for1
+// let k = 12;
+// let n = 3;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// for2
+
+// let narx = 5000;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i} kg = ${i * narx}`);
+// }
+
+// for3
+
+// let n = 5;
+// let counter = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     counter += (n + i) ** 2;
+// }
+// console.log("Counter:", counter);
+
+//for4
+
+// let n = 5;
+// let k = n;
+// let counter = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   counter += i ** k;
+//   k--;
+// }
+
+// console.log(counter);
+
+// For5
+
+// let a = 3
+// let b = 12
+// let text = ''
+
+// for (let i = a; i <= b; i++) {
+//     for (let j = 0; j < i; j++) {
+//         text += i
+//     }
+//     console.log(text)
+//     text = ''
+// }
+
+//Fore6
+
+// let number = 24;
+// for (let i = 1; i <= number; i++) {
+//     if (i, number % i == 0) {
+//         console.log(i)
+//     }
+// }
